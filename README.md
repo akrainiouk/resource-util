1e2687320eebf2c08f521a65bd97056662335e91
Alexei Krainiouk <akrainio@adobe.com>
2019-02-11 20:11:26 -0800

    Added README.md FileUtil changed to utilize CloseableUtil

---
+++ README.md
@@ -0,0 +1,24 @@
+## resource util
+
+Provides miscellaneous resource management utilities
+
+Examples:
+
+1. Using AutoCloseable in for comprehension (copying streams):
+```scala
+import com.agk.resource.CloseableUtil._
+import java.io.{FileInputStream, FileOutputStream}
+for {
+  in <- new FileInputStream("input-file")
+  out <- new FileOutputStream("output-file")
+} {
+  val buffer = new Array[Byte](1024)
+  var n = in.read(buffer)
+  while (n >= 0) {
+    out.write(buffer, 0, n)
+    n = in.read(buffer)
+  }
+}
+```
+
+2. 
--- src/main/scala/com/agk/resource/FileUtil.scala
+++ src/main/scala/com/agk/resource/FileUtil.scala
@@ -1,20 +1,19 @@
 package com.agk.resource
 
 import java.io.File
+import java.io.File.createTempFile
 
+import com.agk.resource.CloseableUtil._
 import org.apache.commons.io.FileUtils.forceDelete
 
 object FileUtil {
 
   def withTempDir[T](prefix: String)(code: File => T): T = {
-    val file = File.createTempFile(prefix, "")
-    file.delete()
-    try {
+    for (file <- createTempFile(prefix, "").withCleanup(forceDelete)) yield {
+      file.delete()
       file.mkdirs()
       code(file)
-    } finally {
-      forceDelete(file)
     }
   }
-
+  
 }
